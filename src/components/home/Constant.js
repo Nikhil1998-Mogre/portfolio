44194487a91a3081f318a6d77e0cbb7d546a32ae
nikhil <nihkilmogre1998@gmail.com>
2024-07-14 10:51:27 +0530

    initial commit

---
+++ src/components/home/Constant.js
@@ -0,0 +1,39 @@
+
+export const PROJECTS = [
+    {
+       title: 'Text to Sql using Gemini',
+      logo: require("../../assets/1_0p0C_mqIXsXmZPpKqxeNtA.png"),
+       description: 'Used Gemini API to convert Text to Query for Structured Data',
+       repolink: 'https://github.com/Nikhil1998-Mogre/End-to-End-Gen-AI-Project-using-Google-Gemini-Pro_text_to_sql'
+    },
+    {
+        title: 'Llama with RAG',
+        logo:require("../../assets/logo_for_llama_with_rag.jpeg"),
+        description: 'RAG Application',
+        repolink: 'https://github.com/Nikhil1998-Mogre/LLama-with-RAG'
+     },
+     {
+        title: 'Real Time Data Engineering (Azure)',
+        logo: require("../../assets/5730634_acba_3.jpg"),
+        description: 'End to End Real time Project from Extraction, loading, transformation and creating Dashoard(PowerBI)',
+        repolink: 'https://github.com/Nikhil1998-Mogre/End-to-End-Relatime-Data-Engineering-project'
+     },
+     {
+        title: 'Sign Language',
+        logo: require("../../assets/sign_lang.jpg"),
+        description: 'Object detection using Yolo end to end',
+        repolink: 'https://github.com/Nikhil1998-Mogre/sign_language_project_yolov5/tree/main'
+     },
+     {
+        title: 'Text Summarization',
+        logo: require("../../assets/1_gYVZCuL7dz8ALoyp7Gob_A.png"),
+        description: 'Using Hugging Face model and deploying it using AWS',
+        repolink: 'https://github.com/Nikhil1998-Mogre/Text-Summarizer-Project'
+     },
+     {
+        title: 'Movie Recommendation',
+        logo: require("../../assets/movie_logo.png"),
+        description: 'Using Django with end to end i,e you will receive mail after login also movie review for each movie',
+        repolink: 'https://github.com/Nikhil1998-Mogre/Movie_Recom_system_Django-2'
+     },
+]
---
+++ src/components/home/Project.jsx
@@ -0,0 +1,88 @@
+import React, { useState, useEffect, useCallback } from "react";
+import Container from "react-bootstrap/Container";
+import { Jumbotron } from "./migration";
+import Row from "react-bootstrap/Row";
+import ProjectCard from "./ProjectCard";
+import axios from "axios";
+import { Card, Col } from "react-bootstrap";
+import Skeleton from "react-loading-skeleton";
+import { PROJECTS } from "./Constant";
+
+const dummyProject = {
+  name: null,
+  description: null,
+  svn_url: null,
+  stargazers_count: null,
+  languages_url: null,
+  pushed_at: null,
+};
+const API = "https://api.github.com";
+// const gitHubQuery = "/repos?sort=updated&direction=desc";
+// const specficQuerry = "https://api.github.com/repos/hashirshoaeb/";
+
+const Project = ({ heading, username, length, specfic }) => {
+  const allReposAPI = `${API}/users/${username}/repos?sort=updated&direction=desc`;
+  const specficReposAPI = `${API}/repos/${username}`;
+  const dummyProjectsArr = new Array(length + specfic.length).fill(
+    dummyProject
+  );
+
+  const [projectsArray, setProjectsArray] = useState([]);
+
+  const fetchRepos = useCallback(async () => {
+    let repoList = [];
+    try {
+      // getting all repos
+      const response = await axios.get(allReposAPI);
+      // slicing to the length
+      repoList = [...response.data.slice(0, length)];
+      // adding specified repos
+      try {
+        for (let repoName of specfic) {
+          const response = await axios.get(`${specficReposAPI}/${repoName}`);
+          repoList.push(response.data);
+        }
+      } catch (error) {
+        console.error(error.message);
+      }
+      // setting projectArray
+      // TODO: remove the duplication.
+      setProjectsArray(repoList);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }, [allReposAPI, length, specfic, specficReposAPI]);
+
+  useEffect(() => {
+    fetchRepos();
+  }, [fetchRepos]);
+
+  return (
+    <Jumbotron fluid id="projects" className="bg-light m-0">
+      <Container className="">
+        <h2 className="display-4 pb-5 text-center">{heading}</h2>
+        <div style={{ display: "grid", gridTemplateColumns: 'repeat(2, minmax(0, 1fr))', gap: '1rem' }} >
+          {
+            PROJECTS.map(({ title, logo, description, repolink }) => (
+              <div style={{ backgroundColor: 'white', padding: '1rem', borderRadius: '6px', boxShadow: 'rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px' }} >
+                <div style={{ fontSize: '18px', fontWeight: 'bold' }} >{title}</div>
+                <div style={{ display: 'flex', gap: '1rem', padding: '1rem 0px' }} >
+                  <img width={180} height={180} src={logo} alt="" style={{ borderRadius: '6px', objectFit: 'fill', border: '1px solid grey' }} />
+                  <div style={{ fontSize: '14px' }} >{description}</div>
+                </div>
+                <hr />
+                <div>
+                  <a href={repolink} target=" _blank" className="btn btn-outline-secondary mx-2">
+                    <i className="fab fa-github" /> Repo
+                  </a>
+                </div>
+              </div>
+            ))
+          }
+        </div>
+      </Container>
+    </Jumbotron>
+  );
+};
+
+export default Project;
